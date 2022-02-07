b141375dcd61c44609e1d8e0753ab128a38af2c2
Robert Eschbach <robert.eschbach@iits-consulting.de>
2022-02-07 16:49:42 +0100

    l2 challenge

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'Fabulous socks of amazing quality!'
         }
     }
 })
